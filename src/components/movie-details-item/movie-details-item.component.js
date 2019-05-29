cb274aee247e35a5f4df42c57de255e88298588b
Illia <ilyas.sporichev@gmail.com>
2019-05-29 14:19:26 +0300

    details page integrated

--- src/components/movie-details-item/movie-details-item.component.js
+++ src/components/movie-details-item/movie-details-item.component.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import { Button } from 'antd';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router';
+import withStyles from 'isomorphic-style-loader/withStyles'
 import { Link } from 'react-router-dom';
 
-import './styles.scss'
+import styles from './styles.scss'
 
-const MovieDetailsItem = ({ movie: { title, poster_path, vote_average, release_date, runtime, overview }, history: { push } }) => {
+const MovieDetailsItem = ({ movie: { title, poster_path, vote_average, release_date, runtime, overview }}) => {
     return (
-            <div className='movie-details-container'>
-                <div className='movie-details-item'>
-                    <Link to='../' className='back-button'>
+            <div className={styles['movie-details-container']}>
+                <div className={styles['movie-details-item']}>
+                    <Link to='../' className={styles['back-button']}>
                         <Button htmlType='submit'
                                 type='primary'
-                                onClick={() => push('/')}
                         >
                             Search
                         </Button>
                     </Link>
-                    <div className='movie-details-poster'>
+                    <div className={styles['movie-details-poster']}>
                         <img src={poster_path} alt='Movie poster'/>
                     </div>
-                    <div className='movie-details-info'>
-                        <div className='movie-details-heading-line'>
-                            <h3>{title}</h3>
-                            <div className='rate'>{vote_average}</div>
+                    <div className={styles['movie-details-info']}>
+                        <div className={styles['movie-details-heading-line']}>
+                            <h3 className={styles.h3}>{title}</h3>
+                            <div className={styles.rate}>{vote_average}</div>
                         </div>
-                        <div className='movie-details-tech-info'>
-                            <div className='duration'>{release_date && release_date.slice(0, 4)}</div>
-                            <div className='runtime'>{runtime} min</div>
+                        <div className={styles['movie-details-tech-info']}>
+                            <div className={styles.duration}>{release_date && release_date.slice(0, 4)}</div>
+                            <div className={styles.runtime}>{runtime} min</div>
                         </div>
 
                         <p>{overview}</p>
@@ -40,7 +39,6 @@ const MovieDetailsItem = ({ movie: { title, poster_path, vote_average, release_d
 
 MovieDetailsItem.propTypes = {
     movie: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
 };
 
-export default withRouter(MovieDetailsItem);
+export default withStyles(styles)(MovieDetailsItem);
